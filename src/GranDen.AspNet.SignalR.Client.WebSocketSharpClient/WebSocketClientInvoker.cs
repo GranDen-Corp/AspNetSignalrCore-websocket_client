65a18da8d3bc1c701862f40a611f3c956740a109
Chen Yu Pao <windperson@gmail.com>
2018-08-08 20:05:58 +0800

    need to use injectable Logger of WebSocket constructor

---
+++ src/GranDen.AspNet.SignalR.Client.WebSocketSharpClient/WebSocketClientInvoker.cs
@@ -0,0 +1,43 @@
+﻿
+using System;
+using WebSocketSharp;
+
+namespace GranDen.AspNet.SignalR.Client.WebSocketSharpClient
+{
+    public class WebSocketClientInvoker
+    {
+        private WebSocket _webSocket;
+        private Serilog.ILogger _logger;
+
+        public WebSocketClientInvoker(string url)
+        {
+            _webSocket = new WebSocket(url) {EnableRedirection = true};
+            _webSocket.Log 
+        }
+
+        ~WebSocketClientInvoker()
+        {
+            var socket = _webSocket as IDisposable;
+            socket?.Dispose();
+        }
+        
+        #region API Methods
+
+        public void Connect()
+        {
+            _webSocket.Connect();
+        }
+
+        public void Close()
+        {
+            _webSocket.Close();
+        }
+        
+       #endregion 
+        
+        
+        
+        
+
+    }
+}
